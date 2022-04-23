3708a51f0546ab8aa33c28cff912e860e1c64fa6
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2022-04-23 17:33:01 +0200

    added get, create, update and delete endpoints

--- src/main/java/com/example/aplikacjeBackendowe3/UsersController.java
+++ src/main/java/com/example/aplikacjeBackendowe3/UsersController.java
@@ -1,10 +1,14 @@
 package com.example.aplikacjeBackendowe3;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.Map;
 
 @Controller
 public class UsersController {
@@ -14,9 +18,51 @@ public class UsersController {
 
     @RequestMapping("/api/users")
     public ResponseEntity<UsersPage> getUsers(
-            @RequestParam(name = "page-number", defaultValue = "1") int pageNumber,
-            @RequestParam(name = "page-size", defaultValue = "20") int pageSize
+            @RequestParam(name = "page-number", defaultValue = "1") @Min(1) int pageNumber,
+            @RequestParam(name = "page-size", defaultValue = "20") @Min(1) @Max(100) int pageSize
     ){
         return ResponseEntity.ok(this.usersService.getUsers(pageNumber, pageSize));
+    }
+
+    @RequestMapping(
+            value = "/api/user/create",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public ResponseEntity<UsersEntity> createUser(@RequestBody UsersEntity user){
+        return ResponseEntity.ok(this.usersService.addUser(user));
+    }
+
+    @RequestMapping(
+            value = "/api/users/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public ResponseEntity<UsersEntity> getUser(@PathVariable("id") int id){
+        return ResponseEntity.ok(this.usersService.getUser(id));
+    }
+
+    @RequestMapping(
+            value = "/api/users/{id}/update",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public ResponseEntity<UsersEntity> updateUser(@PathVariable("id") int id, @RequestBody UsersEntity user){
+        return ResponseEntity.ok(this.usersService.updateUser(id, user));
+    }
+
+    @RequestMapping(
+            value = "/api/users/{id}/remove",
+            method = RequestMethod.DELETE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable("id") int id){
+        return ResponseEntity.ok(this.usersService.deleteUser(id));
     }
 }
--- src/main/java/com/example/aplikacjeBackendowe3/UsersService.java
+++ src/main/java/com/example/aplikacjeBackendowe3/UsersService.java
@@ -3,24 +3,53 @@ package com.example.aplikacjeBackendowe3;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class UsersService {
 
+    List<UsersEntity> users = new ArrayList<>();
+
+    public UsersService() {
+        users.add(new UsersEntity(1, "adam"));
+        users.add(new UsersEntity(2, "john"));
+        users.add(new UsersEntity(3, "jake"));
+        users.add(new UsersEntity(4, "mia"));
+    }
+
     public UsersPage getUsers (int pageNumber, int pageSize){
-        List<UsersEntity> users = new ArrayList<>();
-        users.add(new UsersEntity(1,"john"));
-        users.add(new UsersEntity(2, "jake"));
-        users.add(new UsersEntity(3, "mia"));
+
         int totalCount = users.size();
         int pageCount = calculatePagesCount(pageSize, totalCount);
 
         return new UsersPage(pageNumber, pageCount, pageSize, totalCount, users);
     }
 
+    public UsersEntity addUser (UsersEntity user) {
+        user.setId(users.size() + 1);
+        users.add(user);
+        return user;
+    }
+
     public int calculatePagesCount(int pageSize, int totalCount) {
 
         return totalCount < pageSize ? 1 : (int) Math.ceil((double) totalCount / (double) pageSize);
+    }
+
+    public UsersEntity getUser(int id) {
+        return users.get(id - 1);
+    }
+
+    public Map<String, Boolean> deleteUser(int id) {
+        users.remove(id - 1);
+        return Collections.singletonMap("result", true);
+    }
+
+    public UsersEntity updateUser(int id, UsersEntity user) {
+        user.setId(id);
+        users.set(id - 1, user);
+        return user;
     }
 }
